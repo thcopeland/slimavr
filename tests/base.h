6e0106e6dc85c729eb5869ea0106a923f7ab7417
Tom Copeland <thcopeland2001@gmail.com>
2022-07-31 00:38:04 -0400

    Test 16-bit timer output
    The tests are not nearly so comprehensive as the 8-bit timer tests,
    but should be enough, since the code is nearly identical.

--- tests/base.h
+++ tests/base.h
@@ -3,18 +3,18 @@
 
 void run_test(char *fname, long duration, struct avr_model model) {
     struct avr *avr = avr_init(model);
-    FILE *f1 = fopen("../pb7.dat", "w"),
-         *f2 = fopen("../tcnt0.dat", "w"),
-         *f3 = fopen("../pg5.dat", "w");
+    FILE *f1 = fopen("../ch1.dat", "w"),
+         *f2 = fopen("../tcnt.dat", "w"),
+         *f3 = fopen("../ch2.dat", "w");
     long c = 0;
     if (avr_load_ihex(avr, fname) == 0) {
         for (long i = 0; i < duration; i++) {
             avr_step(avr);
 
-            // if (c > 0 || avr->reg[0x45]) c++;
-            // fprintf(f1, "%lu\t%d\n", c, (avr->mem[0x25] > 0) * 80);
-            // fprintf(f3, "%lu\t%d\n", c, (avr->mem[0x34] > 0) * 80);
-            // fprintf(f2, "%lu\t%d\n", c, avr->mem[0x46]);
+            // if (c > 0 || avr->reg[0x81]) c++;
+            // fprintf(f1, "%lu\t%d\n", c, ((avr->mem[0x25] & 0x20) > 0) * 25000);
+            // fprintf(f3, "%lu\t%d\n", c, ((avr->mem[0x25] & 0x40) > 0) * 30000);
+            // fprintf(f2, "%lu\t%d\n", c, avr->mem[0x84] | (avr->mem[0x85] << 8));
 
             if (avr->status == CPU_STATUS_CRASHED) {
                 printf("cpu crashed (%d)\n", avr->error);
--- tests/test_timers.c
+++ tests/test_timers.c
@@ -18,14 +18,15 @@ void test_16bit_timer_interrupts(void) {
     run_test("asm/timer_16bit_interrupts.hex", 800000, MODEL_ATMEGA1280);
 }
 
-// void test_16bit_timer_output(void) {
-//     printf("testing 16 bit timers (part 2)... ");
-//     run_test("asm/timer_16bit_output.hex", 1450, MODEL_ATMEGA1280);
-// }
+void test_16bit_timer_output(void) {
+    printf("testing 16 bit timers (part 2)... ");
+    run_test("asm/timer_16bit_output.hex", 120000, MODEL_ATMEGA1280);
+}
 
 int main(int argc, char **argv) {
-    // test_8bit_timer_interrupts();
-    // test_8bit_timer_output();
+    test_8bit_timer_interrupts();
+    test_8bit_timer_output();
     test_16bit_timer_interrupts();
+    test_16bit_timer_output();
     return 0;
 }
